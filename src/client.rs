51a6207aea11cd79167e4306c7ac525808e739ac
Esteban Borai <estebanborai@gmail.com>
2022-07-03 17:35:01 -0400

    chore: initial commit

---
+++ src/client.rs
@@ -0,0 +1,72 @@
+pub const UPLOAD_ENDPOINT: &'static str = "https://upload.imagekit.io/api/v1/files/upload";
+
+/// An ImageKit.io API Client Instance
+///
+/// Requires a PublicKey, PrivateKey and URL Endpoint from your ImageKit
+/// account.
+///
+/// By default the `upload_endpoint` value used is the `UPLOAD_ENDPOINT`
+/// constant:
+///
+/// ```ignore
+/// https://upload.imagekit.io/api/v1/files/upload
+/// ```
+///
+/// If you want to set a custom upload endpoint, you can use the
+/// `upload_endpoint` method.
+pub struct ImageKit {
+    pub(crate) upload_endpoint: String,
+    pub(crate) public_key: String,
+    pub(crate) private_key: String,
+    pub(crate) url_endpoint: String,
+}
+
+impl ImageKit {
+    pub fn new<T: ToString>(public_key: T, private_key: T, url_endpoint: T) -> Self {
+        Self {
+            upload_endpoint: UPLOAD_ENDPOINT.to_string(),
+            public_key: public_key.to_string(),
+            private_key: private_key.to_string(),
+            url_endpoint: url_endpoint.to_string(),
+        }
+    }
+
+    /// Updates the `upload_endpoint` used for this client instance.
+    ///
+    /// ```
+    /// use imagekit::client::ImageKit;
+    ///
+    /// let mut image_kit = ImageKit::new(
+    ///    "your_public_api_key",
+    ///    "your_private_api_key",
+    ///    "https://ik.imagekit.io/your_imagekit_id/",
+    /// );
+    /// let new_endpoint = String::from("https://upload.example.com/api/v1/files/upload");
+    ///
+    /// *image_kit.upload_endpoint() = new_endpoint.clone();
+    ///
+    /// assert_eq!(image_kit.upload_endpoint().to_owned(), new_endpoint);
+    /// ```
+    pub fn upload_endpoint(&mut self) -> &mut String {
+        &mut self.upload_endpoint
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ImageKit;
+
+    #[test]
+    fn it_updates_the_upload_endpoint() {
+        let mut image_kit = ImageKit::new(
+            "your_public_api_key",
+            "your_private_api_key",
+            "https://ik.imagekit.io/your_imagekit_id/",
+        );
+        let new_endpoint = String::from("https://upload.example.com/api/v1/files/upload");
+
+        *image_kit.upload_endpoint() = new_endpoint.clone();
+
+        assert_eq!(image_kit.upload_endpoint, new_endpoint);
+    }
+}
